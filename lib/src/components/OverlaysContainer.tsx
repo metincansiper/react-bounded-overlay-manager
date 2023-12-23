0da3badb0b42649837249e4c5943824f6b73b3d6
metincansiper <metincansiper@gmail.com>
2023-12-23 20:00:49 +0300

    Added optional unmountContentWhenHidden prop to OverlaysContainer

--- lib/src/components/OverlaysContainer.tsx
+++ lib/src/components/OverlaysContainer.tsx
@@ -4,15 +4,25 @@ import Overlay from './Overlay';
 type Props = {
     children: ReactElement<typeof Overlay>[] | ReactElement<typeof Overlay>,
     show: boolean,
+    unmountContentWhenHidden?: boolean,
 }
 
 const OverlaysContainer: React.ForwardRefRenderFunction<HTMLDivElement, Props> = 
-({ children, show }, ref) => {
+({ children, show, unmountContentWhenHidden = false }, ref) => {
+    const display = show ? 'block' : 'none'
+    const shouldUnmount = unmountContentWhenHidden && !show;
+
+    const content = shouldUnmount ? null : (
+        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+            { children }
+        </div>
+    );
+    
     return (
-        <div ref={ref as ForwardedRef<HTMLDivElement>} style={{ position: 'absolute', display: show ? 'block' : 'none', pointerEvents: 'none' }}>
-            <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-                { children }
-            </div>
+        <div ref={ref as ForwardedRef<HTMLDivElement>} style={{ position: 'absolute', display, pointerEvents: 'none' }}>
+            {
+                content
+            }
         </div>
     );
 };
