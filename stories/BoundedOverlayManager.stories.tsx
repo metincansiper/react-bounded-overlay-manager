317efdbcb39044c7499956ac6105b41fee8e8405
metincansiper <metincansiper@gmail.com>
2023-12-28 22:15:21 +0300

    Set up storybook and adding the stories

---
+++ stories/BoundedOverlayManager.stories.tsx
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react';
+import type { Meta, Story } from '@storybook/react';
+import BoundedOverlayManager, { Overlay, PredefinedPosition } from '../lib/main';
+
+const meta: Meta = {
+  title: 'Components/BoundedOverlayManager',
+  component: BoundedOverlayManager,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    persistentlyShowOverlays: {
+      control: 'boolean',
+      description: 'If true, overlays will be shown persistently',
+    },
+  },
+};
+
+export default meta;
+
+const BoundedOverlayManagerStory: Story = (args) => {
+  const boundingComponentRef = useRef(null);
+
+  return (
+    <div ref={boundingComponentRef} style={{ width: '300px', height: '200px', border: '1px solid black' }}>
+      <BoundedOverlayManager boundingComponentRef={boundingComponentRef} {...args}>
+        <Overlay position={PredefinedPosition.CENTER}>
+          <div>Hello World</div>
+        </Overlay>
+      </BoundedOverlayManager>
+    </div>
+  );
+};
+
+export const Default = BoundedOverlayManagerStory.bind({});
+
+// CustomBoundedOverlayManager.args = {
+//   persistentlyShowOverlays: true,
+// };
