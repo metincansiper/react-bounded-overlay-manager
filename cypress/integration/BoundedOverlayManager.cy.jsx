39f0aeccc32f1edf48307819159dbe21a96c1782
metincansiper <metincansiper@gmail.com>
2023-12-31 10:07:30 +0300

    Implementing cypress react tests for boundedoverlaymanager

--- cypress/integration/BoundedOverlayManager.cy.jsx
+++ cypress/integration/BoundedOverlayManager.cy.jsx
@@ -1,11 +1,9 @@
 import React, { createRef, useRef } from 'react';
 import { mount } from '@cypress/react18';
 import BoundedOverlayManager, { Overlay, PredefinedPosition } from '../../lib/main';
-import cssUnits from 'units-css';
 
 describe('OverlayManagerTest Component', () => {
-    it('overlay content is visible', () => {
-        // let boundingComponentRef = createRef();
+    it('overlays container has the right bbox when the bounding component is absolutely positioned inside a relatively positioned parent div', () => {
         const OverlayManagerTest = () => {
             const boundingComponentRef = useRef();
             return (
@@ -15,8 +13,8 @@ describe('OverlayManagerTest Component', () => {
                     </div>
                     
                     <BoundedOverlayManager boundingComponentRef={boundingComponentRef} persistentlyShowOverlays={true}>
-                        <Overlay position={PredefinedPosition.MID_LEFT} offset={{left: '10%', top: '10%'}}>
-                            <div id="overlay-content">Hello World</div>
+                        <Overlay position={PredefinedPosition.TOP_LEFT}>
+                            Hello World
                         </Overlay>
                     </BoundedOverlayManager>
                 </>
@@ -25,42 +23,17 @@ describe('OverlayManagerTest Component', () => {
 
         mount(<OverlayManagerTest />);
         
-        cy.get('#overlay-content').then(($overlayContent) => {
-            const overlayContent = $overlayContent[0];
-            cy.get('#bounding-component').should(($boundingComponent) => {
-                const boundingComponent = $boundingComponent[0];
-                // expect(boundingComponent).to.have.property('width', '400px');
-                expect(cssUnits.convert('px', '10%', overlayContent, 'width')).to.be.a('number');
-                expect(cssUnits.convert('px', '10%', overlayContent, 'height')).to.be.a('number');
-                const offsetLeftInPx = cssUnits.convert('px', '10%', boundingComponent, 'left');
-                const offsetTopInPx = cssUnits.convert('px', '60%', boundingComponent, 'top');
-                
-                const bboxOverlayContent = overlayContent.getBoundingClientRect();
-                const bboxBoundingComponent = boundingComponent.getBoundingClientRect();
+        cy.get('#bounding-component').then(($boundingComponent) => {
+            const boundingComponent = $boundingComponent[0];
+            cy.get('[role="overlays-container"]').should(($overlaysContainer) => {
+                const overlaysContainer = $overlaysContainer[0];
+                const overlaysContainerBbox = overlaysContainer.getBoundingClientRect();
+                const boundingComponentBbox = boundingComponent.getBoundingClientRect();
 
-                // expect(bboxBoundingComponent.left + offsetLeftInPx).to.be.equal(bboxOverlayContent.left);
-                // expect(bboxBoundingComponent.top + offsetTopInPx).to.be.equal(bboxOverlayContent.top);
-                // cy.should(() => {
-                    
-                //     // expect(offsetLeftInPx).to.be.a('number');
-                //     // expect(offsetTopInPx).to.be.a('number');
-                //     // expect(bboxOverlayContent.left).to.be.a('number');
-                //     // expect(bboxOverlayContent.top).to.be.a('number');
-                // });
-
-
-                // cy.wait(2000).then(() => {
-                //     expect(bboxBoundingComponent.left).to.be.a('number');
-                //     expect(bboxBoundingComponent.top).to.be.a('number');
-                //     // expect(offsetLeftInPx).to.be.a('number');
-                //     // expect(offsetTopInPx).to.be.a('number');
-                //     expect(bboxOverlayContent.left).to.be.a('number');
-                //     expect(bboxOverlayContent.top).to.be.a('number');
-                // });
-                
-                
-                // expect(bboxBoundingComponent.left).to.be.equal(bboxOverlayContent.left + offsetLeftInPx);
-                // expect(bboxBoundingComponent.top).to.be.equal(bboxOverlayContent.top + offsetTopInPx);
+                expect(overlaysContainerBbox.left).to.be.equal(boundingComponentBbox.left);
+                expect(overlaysContainerBbox.top).to.be.equal(boundingComponentBbox.top);
+                expect(overlaysContainerBbox.width).to.be.equal(boundingComponentBbox.width);
+                expect(overlaysContainerBbox.height).to.be.equal(boundingComponentBbox.height);
             });
         });
         
