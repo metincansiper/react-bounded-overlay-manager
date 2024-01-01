46d6326e9ba5859e740e3f70736e8f81d4380bde
metincansiper <metincansiper@gmail.com>
2024-01-01 23:00:46 +0300

    Integration tested that bounding components content must be blocked only by the overlays themselves not the overlays container

--- cypress/integration/BoundedOverlayManager.cy.tsx
+++ cypress/integration/BoundedOverlayManager.cy.tsx
@@ -6,12 +6,14 @@ const boundingComponentId = 'bounding-component';
 const getBoundingComponent = () => cy.get(`#${boundingComponentId}`);
 const getOverlaysContainer = () => cy.get('[role="overlays-container"]');
 
-const OverlayManagerTest = ({ boundingComponentStyle = {}, boundingComponentParentStyle = {}, overlayManagerProps = {}, overlayProps = {} }) => {
+const OverlayManagerTest = ({ boundingComponentChildren = null, boundingComponentStyle = {}, boundingComponentParentStyle = {}, overlayManagerProps = {}, overlayProps = {} }: any) => {
     const boundingComponentRef = useRef(null);
     return (
         <>
             <div style={boundingComponentParentStyle}>
-                <div id={boundingComponentId} style={boundingComponentStyle} ref={boundingComponentRef}></div>
+                <div id={boundingComponentId} style={boundingComponentStyle} ref={boundingComponentRef}>
+                    {boundingComponentChildren}
+                </div>
             </div>
 
             <BoundedOverlayManager boundingComponentRef={boundingComponentRef} {...overlayManagerProps}>
@@ -209,26 +211,94 @@ describe('BoundedOverlayManager Component', () => {
         });
     });
 
+    describe('overlays container does not block the children of the bounding component', () => {
+        [
+            {
+                overlayPosition: PredefinedPosition.BOTTOM_RIGHT,
+                description: 'child of the bounding component is clickable when there is no overlay covering it',
+                childShouldBeClicked: true,
+            },
+            {
+                overlayPosition: PredefinedPosition.TOP_LEFT,
+                description: 'child of the bounding component is not clickable when the overlay covers the child',
+                childShouldBeClicked: false,
+            }
+        ].forEach(({ overlayPosition, description, childShouldBeClicked }) => {
+            it(description, () => {
+                const boundingComponentChildId = 'bounding-component-child';
+
+                const boundingComponentStyle = { width: '800px', height: '800px', position: 'relative' };
+                const overlayManagerProps = {
+                    persistentlyShowOverlays: true,
+                };
+                const clickSpy = cy.spy().as('clickSpy');
+                const boundingComponentChildren = (
+                    <div
+                        id={boundingComponentChildId}
+                        onClick={clickSpy}
+                        style={{
+                            position: 'absolute',
+                            left: 0,
+                            top: 0,
+                            width: '100px',
+                            height: '20px'
+                        }}>
+                    </div>
+                );
+                const overlayProps = {
+                    position: overlayPosition,
+                };
+                const CustomOverlayManagerTest = () => {
+                    return (
+                        <OverlayManagerTest
+                            overlayManagerProps={overlayManagerProps}
+                            boundingComponentChildren={boundingComponentChildren}
+                            boundingComponentStyle={boundingComponentStyle}
+                            overlayProps={overlayProps}
+                        />
+                    );
+                };
+
+                mount(<CustomOverlayManagerTest />);
+
+                cy.get(`#${boundingComponentChildId}`).then($boundingComponentChild => {
+                    const boundingRect = $boundingComponentChild[0].getBoundingClientRect();
+                    const x = boundingRect.left + 20;
+                    const y = boundingRect.top + 10;
+
+                    cy.get('body').click(x, y);
+
+                    if (childShouldBeClicked) {
+                        cy.get('@clickSpy').should('have.been.called');
+                    }
+                    else {
+                        cy.get('@clickSpy').should('not.have.been.called');
+                    }
+                });
+            });
+        });
+    });
+
     describe('overlays container has the right bbox after api.updateOverlaysContainerBoundingBox is called ', () => {
         it('works correctly when the bounding component itself is repositioned and then api.updateOverlaysContainerBoundingBox is called', () => {
             let api;
-    
+
             const onApiUpdated = (_api) => {
                 console.log('api up')
                 api = _api;
             };
-    
+
             const boundingComponentStyle = { position: 'absolute', left: 0, top: 0, width: '400px', height: '200px' };
             const boundingComponentParentStyle = { width: '1000px', height: '500px', position: 'relative' };
-    
+
             const CustomOverlayManagerTest = () => {
                 return (
                     <OverlayManagerTest overlayManagerProps={{ onApiUpdated }} boundingComponentStyle={boundingComponentStyle} boundingComponentParentStyle={boundingComponentParentStyle} />
                 );
             }
-    
+
             mount(<CustomOverlayManagerTest />);
-    
+
             getBoundingComponent()
                 .then(($boundingComponent) => {
                     const boundingComponent = $boundingComponent[0];
