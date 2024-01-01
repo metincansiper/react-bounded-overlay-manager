dad22c16086a6aef5865fb278109c55a43167f59
metincansiper <metincansiper@gmail.com>
2024-01-01 17:25:21 +0300

    Added cypress test cases checking if overlays container is resized as expected when the bounding component is resized directly or indirectly

--- cypress/integration/BoundedOverlayManager.cy.jsx
+++ cypress/integration/BoundedOverlayManager.cy.jsx
@@ -3,6 +3,42 @@ import { mount } from '@cypress/react18';
 import BoundedOverlayManager, { Overlay, PredefinedPosition } from '../../lib/main';
 
 describe('BoundedOverlayManager Component', () => {
+    const boundingComponentId = 'bounding-component';
+    const getBoundingComponent = () => cy.get(`#${boundingComponentId}`);
+    const getOverlaysContainer = () => cy.get('[role="overlays-container"]');
+
+    const OverlayManagerTest = ({ boundingComponentStyle = {}, boundingComponentParentStyle = {}, overlayManagerProps = {}, overlayProps = {} }) => {
+        const boundingComponentRef = useRef();
+        return (
+            <>
+                <div style={boundingComponentParentStyle}>
+                    <div id={boundingComponentId} style={boundingComponentStyle} ref={boundingComponentRef}></div>
+                </div>
+    
+                <BoundedOverlayManager boundingComponentRef={boundingComponentRef} persistentlyShowOverlays={true} { ...overlayManagerProps }>
+                    <Overlay position={PredefinedPosition.TOP_LEFT} {...overlayProps}>
+                        Hello World
+                    </Overlay>
+                </BoundedOverlayManager>
+            </>
+        );
+    };
+
+    const expectRightOverlaysContainerBBox = () => {
+        getBoundingComponent().then(($boundingComponent) => {
+            const boundingComponent = $boundingComponent[0];
+            getOverlaysContainer().should(($overlaysContainer) => {
+                const overlaysContainer = $overlaysContainer[0];
+                const overlaysContainerBbox = overlaysContainer.getBoundingClientRect();
+                const boundingComponentBbox = boundingComponent.getBoundingClientRect();
+
+                expect(overlaysContainerBbox.left).to.be.equal(boundingComponentBbox.left);
+                expect(overlaysContainerBbox.top).to.be.equal(boundingComponentBbox.top);
+                expect(overlaysContainerBbox.width).to.be.equal(boundingComponentBbox.width);
+                expect(overlaysContainerBbox.height).to.be.equal(boundingComponentBbox.height);
+            });
+        });
+    };
     describe('overlays container has the right bbox on mount', () => {
         const parentStyles = [
             { position: 'relative', top: '10px', left: '10px' },
@@ -14,7 +50,7 @@ describe('BoundedOverlayManager Component', () => {
             { display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' },
             { overflow: 'hidden', padding: '20px' },
         ];
-        
+
         const boundingStyles = [
             { position: 'absolute', top: '100px', left: '100px', width: '400px', height: '200px' },
             { position: 'relative', top: '50px', left: '150px', width: '300px', height: '100px' },
@@ -24,44 +60,96 @@ describe('BoundedOverlayManager Component', () => {
             { position: 'static', width: '200%', height: '250%' },
             { display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' },
         ];
-    
+
         parentStyles.forEach(parentStyle => {
             boundingStyles.forEach(boundingStyle => {
                 it(`works correctly with parent style ${JSON.stringify(parentStyle)} and bounding style ${JSON.stringify(boundingStyle)}`, () => {
-                    const OverlayManagerTest = () => {
-                        const boundingComponentRef = useRef();
+                    const boundingComponentStyle = { ...boundingStyle };
+                    const boundingComponentParentStyle = { width: '1000px', height: '500px', ...parentStyle };
+                    const CustomOverlayManagerTest = () => {
                         return (
-                            <>  
-                                <div style={{ width: '1000px', height: '500px', ...parentStyle }}>
-                                    <div id='bounding-component' style={{ backgroundColor: 'blue', ...boundingStyle }} ref={boundingComponentRef}></div>
-                                </div>
-                                
-                                <BoundedOverlayManager boundingComponentRef={boundingComponentRef} persistentlyShowOverlays={true}>
-                                    <Overlay position={PredefinedPosition.TOP_LEFT}>
-                                        Hello World
-                                    </Overlay>
-                                </BoundedOverlayManager>
-                            </>
+                            <OverlayManagerTest boundingComponentStyle={boundingComponentStyle} boundingComponentParentStyle={boundingComponentParentStyle} />
                         );
                     };
-    
-                    mount(<OverlayManagerTest />);
-                    
-                    cy.get('#bounding-component').then(($boundingComponent) => {
-                        const boundingComponent = $boundingComponent[0];
-                        cy.get('[role="overlays-container"]').should(($overlaysContainer) => {
-                            const overlaysContainer = $overlaysContainer[0];
-                            const overlaysContainerBbox = overlaysContainer.getBoundingClientRect();
-                            const boundingComponentBbox = boundingComponent.getBoundingClientRect();
-            
-                            expect(overlaysContainerBbox.left).to.be.equal(boundingComponentBbox.left);
-                            expect(overlaysContainerBbox.top).to.be.equal(boundingComponentBbox.top);
-                            expect(overlaysContainerBbox.width).to.be.equal(boundingComponentBbox.width);
-                            expect(overlaysContainerBbox.height).to.be.equal(boundingComponentBbox.height);
-                        });
-                    });
+
+                    mount(<CustomOverlayManagerTest />);
+
+                    expectRightOverlaysContainerBBox();
                 });
             });
         });
-    });  
+    });
+
+    describe('overlays container has the right bbox when bounding component is resized', () => {
+        [
+            {
+                initialDimentions: { width: '400px', height: '200px' },
+                finalDimentions: { width: '600px', height: '300px' }
+            },
+            {
+                initialDimentions: { width: '80vw', height: '100vh' },
+                finalDimentions: { width: '100vw', height: '200vh' }
+            },
+            {
+                initialDimentions: { width: '75%', height: '75%' },
+                finalDimentions: { width: '80%', height: '60%' }
+            }
+        ].forEach(({ initialDimentions, finalDimentions }) => {
+            it(`works correctly when bounding component itself is resized directly from ${JSON.stringify(initialDimentions)} to ${JSON.stringify(finalDimentions)}`, () => {
+                const CustomOverlayManagerTest = () => {
+                    return (
+                        <OverlayManagerTest boundingComponentStyle={ { ...initialDimentions } } />
+                    );
+                }
+    
+                mount(<CustomOverlayManagerTest />);
+    
+                expectRightOverlaysContainerBBox();
+    
+                getBoundingComponent().then(($boundingComponent) => {
+                    const boundingComponent = $boundingComponent[0];
+                    boundingComponent.style.width = finalDimentions.width;
+                    boundingComponent.style.height = finalDimentions.height;
+    
+                    expectRightOverlaysContainerBBox();
+                });
+            });
+        });
+        
+        [
+            {
+                initialDimentions: { width: '400px', height: '200px' },
+                finalDimentions: { width: '600px', height: '300px' }
+            },
+            {
+                initialDimentions: { width: '80vw', height: '100vh' },
+                finalDimentions: { width: '100vw', height: '200vh' }
+            },
+            {
+                initialDimentions: { width: '75%', height: '75%' },
+                finalDimentions: { width: '80%', height: '60%' }
+            },
+        ].forEach(({ initialDimentions, finalDimentions }) => {
+            it(`works correctly when bounding component is resized indirectly through resize of its parent from ${JSON.stringify(initialDimentions)} to ${JSON.stringify(finalDimentions)}`, () => {
+                const CustomOverlayManagerTest = () => {
+                    return (
+                        <OverlayManagerTest boundingComponentParentStyle={ { ...initialDimentions } } />
+                    );
+                };
+
+                mount(<CustomOverlayManagerTest />);
+    
+                expectRightOverlaysContainerBBox();
+    
+                getBoundingComponent().then(($boundingComponent) => {
+                    const boundingComponentParent = $boundingComponent[0].parentNode;
+                    boundingComponentParent.style.width = finalDimentions.width;
+                    boundingComponentParent.style.height = finalDimentions.height;
+    
+                    expectRightOverlaysContainerBBox();
+                });
+            });
+        });
+        
+    });
 });
