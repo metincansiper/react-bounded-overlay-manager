f367e41475da142a41656ef1c1892928371def08
metincansiper <metincansiper@gmail.com>
2024-01-03 17:56:04 +0300

    Updates

--- assets/preview-VakRj62I.js
+++
@@ -1,1 +0,0 @@
-const e="/react-bounded-overlay-manager/",a={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},publicPath:e}};export{a as default};
