322e39306143c05c6e1c82e187c51ba4a3fa7dae
metincansiper <metincansiper@gmail.com>
2024-01-03 17:30:11 +0300

    Updates

---
+++ assets/preview-LeHcoWpQ.js
@@ -0,0 +1,1 @@
+const e="/react-bounded-overlay-manager/",a={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},baseUrl:e}};export{a as default};
